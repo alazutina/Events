2f1706ed1f4944d03a6f7745865db29f17afc955
Anna Lazutina <alazutina@yandex.ru>
2022-06-26 19:56:29 +0300

    initial commit

---
+++ src/main/java/event/AppRunner.java
@@ -0,0 +1,81 @@
+package event;
+
+import event.controller.EventController;
+import event.controller.FileController;
+import event.controller.UserController;
+import event.model.Event;
+import event.model.User;
+import  event.model.File;
+
+import javax.jws.soap.SOAPBinding;
+import java.awt.*;
+
+public class AppRunner {
+
+
+    public static void main(String[]args){
+   FileController fileController = new FileController();
+
+        File f = new File();
+        f.setPath("ddddd");
+
+        System.out.
+                println(f);
+
+        fileController.save(f.getPath());
+
+        System.out.println(fileController.getById(1l));
+     System.out.println(fileController.getAll());
+
+     // fileController.deleteById(6l);
+
+        System.out.println(fileController.getAll());
+
+        fileController.update(1l, "sss");
+
+        System.out.println(fileController.getAll());
+
+       UserController userController = new UserController();
+
+        User user = new User();
+        user.setName("AAA");
+
+        userController.save(user.getName());
+
+        System.out.println(userController.getAll());
+
+        System.out.println(userController.getById(3l));
+
+        User user1 = userController.getById(3l);
+        user1.setName("ddd00");
+        userController.update(user1.getId(),user1.getName());
+
+      //  userController.deleteById(2l);
+       System.out.println(userController.getAll());
+
+        EventController eventController = new EventController();
+
+        Event event = new Event();
+
+        event.setUser(userController.getById(7l));
+        event.setFile(fileController.getById(7l));
+        event.setAction("открытие");
+        eventController.save(event.getAction(),event.getFile(),event.getUser());
+
+        System.out.println("! "+eventController.getAll());
+    //    eventController.deleteById(42l);
+
+      Event event1 = eventController.getById(43l);
+      event1.setAction("закрытие");
+      eventController.update(event1.getId(),event1.getAction(),event1.getFile(),event1.getUser());
+
+
+        System.out.println("!! "+eventController.getAll());
+        System.out.println(userController.getAll());
+        System.out.println(fileController.getAll());
+
+        System.out.println(eventController.getById(44l));
+
+        System.exit(0);
+    }
+}
